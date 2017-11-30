2704ba029d681301e3a1d74f537404b4e8bf4100
cn7abc <33703959+cn7abc@users.noreply.github.com>
2017-11-29 20:09:18 -0500

    Update project.c
    This is tony stark's part...

--- project.c
+++ project.c
@@ -106,6 +106,41 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
 
+	// if MemRead is 1 Load instruction
+	if(MemRead == 1)
+	{
+
+		// Check if word is aligned by 4 indexes 
+		if(ALUresult %  4 != 0)
+		{
+
+			return 1;
+
+		}
+	
+		// this mulitpes by 4 by shifting  right by 2, and it moves us 4 indexes
+		*memdata = Mem[ALUresult>>2];
+
+	}
+
+	
+	// if MemWrite is 1 we save instruction
+	else if(MemWrite == 1)
+	{
+			
+		// Check if word is aligned by 4 indexes 
+		if(ALUresult %  4 != 0)
+		{
+			return 1;
+		}
+		
+		// this mulitpes by 4 by shifting  right by 2, and it moves us 4 indexes
+		Mem[ALUresult>>2] = data2;
+	
+	}
+
+	return 0;
+
 }
 
 
@@ -113,13 +148,90 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+	// Write to register only if RegWrite is asserted.
+	if(RegWrite == 1)
+	{
+		//  Write to r2. 
+		if( RegDst == 0 )
+		{
+			if(MemtoReg == 1)
+			{
+				// From mem to register r2
+				Reg[r2] = memdata; 
+				return; 
+			}
+		
+			else
+			{
+
+				// we save into r2 registar 
+				Reg[r2] = ALUresult;
+				return;
+
+			}
+		}
+	
+		// Or r3.
+		else
+		{
+				if(MemtoReg == 1)
+		{
+
+				// From mem to register r3
+				Reg[r3] = memdata;
+  
+		}
+
+		else
+)		{
+
+				Reg[r3] = ALUresult;
+
+		}
+	 }
+	}
+
+	return;
 
 }
+
 
 /* PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
 
+	// if it tells us to branch and  zero is 1 do the inside
+	if((Branch == 1) && (Zero == 1))
+	{
+
+		// which the inside here offsets + PC + 4 and it puts  four zeros in the 
+		// most signifcant btye to delay it
+		*PC = ((*PC) + (4) + (extended_value<<2));
+		return;
+
+	}
+
+	// else it told us to jump
+	else if(Jump == 1)
+	{
+
+		// it gets the least sinficant btyes and it sifts them by 2 
+		// PC(31 - 28) and jsec shifted left 2
+		*PC = ((*PC & 0xF0000000) + (jsec<<2);
+		return; 
+	}
+
+	
+
+	// it progresses it by Incrementing it  by 4
+	else
+	{
+
+		// it progresses it by Incrementing it  by 4
+		*PC = (*PC + 4);
+		return;
+
+	}
 }
 
